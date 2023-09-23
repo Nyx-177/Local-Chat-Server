479a47a41d4eb9631d88b2984753333288f5d495
Nyx <necrownyx@outlook.com>
2023-09-23 19:38:10 +1000

    added init to sock.py

--- server/utils/sock.py
+++ server/utils/sock.py
@@ -1,2 +1,16 @@
 class Socket:
-    pass
+    def __init__(self, port, threads=True, handler=print, logging=True):
+        from .log import log
+        import threading
+        import socket
+
+        self.port = port
+        self.handler = handler
+
+        self.connections = []
+        self.lock = threading.Lock()
+
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind(('0.0.0.0', port))
+        self.server_socket.listen(5)
+        log.info(f'Listening on port {port}')
