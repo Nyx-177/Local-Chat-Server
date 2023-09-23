0bc7e51dd922819f47e1dd74568072d46ffd683b
Nyx <necrownyx@outlook.com>
2023-09-23 19:32:31 +1000

    added missing imports to logger

--- server/utils/log.py
+++ server/utils/log.py
@@ -1,9 +1,12 @@
 class log:
     def info(text):
+        import colorama
         print(f'[{colorama.Fore.GREEN}INFO{colorama.Fore.RESET}] {text}')
     
     def warn(text):
+        import colorama
         print(f'[{colorama.Fore.YELLOW}WARNING{colorama.Fore.RESET}] {text}')
     
     def error(text):
+        import colorama
         print(f'[{colorama.Fore.RED}ERROR{colorama.Fore.RESET}] {text}')
