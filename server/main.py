c6ddf71b623f74875c393c50690e2a85546e1bbb
Nyx <necrownyx@outlook.com>
2023-09-24 15:42:55 +1000

    change the rsa class to use the Crypto package

--- server/main.py
+++ server/main.py
@@ -1,56 +1,66 @@
-from utils import log
-
-import rsa  # You can use the 'rsa' library to handle RSA encryption and signing
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
 
 class Rsa:
     def __init__(self, publicKey=None, privateKey=None):
-        self.public_key = publicKey
-        self.private_key = privateKey
+        self.private_key = None
+        self.public_key = None
+
+        if publicKey:
+            self.public_key = RSA.import_key(publicKey)
+        if privateKey:
+            self.private_key = RSA.import_key(privateKey)
+
+        if not self.private_key and not self.public_key:
+            self.private_key = RSA.generate(2048)
+            self.public_key = self.private_key.publickey()
+
+        if not self.private_key and publicKey:
+            self.private_key = False
         
-        if not self.private_key and self.public_key:
-            # Try to extract the public key from the given private key
-            try:
-                self.private_key = rsa.PrivateKey.load_pkcs1(self.public_key)
-            except Exception:
-                self.private_key = None
+        if not self.public_key and privateKey:
+            self.public_key = RSA.import_key(privateKey).publickey()
 
     def decrypt(self, data):
-        try:
-            decrypted_data = rsa.decrypt(data, self.private_key)
-            return decrypted_data
-        except Exception:
+        if self.private_key:
+            try:
+                cipher = PKCS1_OAEP.new(self.private_key)
+                decrypted_data = cipher.decrypt(data)
+                return decrypted_data
+            except Exception:
+                return False
+        else:
             return False
 
     def encrypt(self, data, withPrivate=False):
-        if withPrivate:
-            key_to_use = self.private_key
-        else:
-            key_to_use = self.public_key
-
-        if key_to_use:
-            encrypted_data = rsa.encrypt(data, key_to_use)
-            return encrypted_data
+        if withPrivate and self.private_key:
+            cipher = PKCS1_OAEP.new(self.private_key)
+        elif self.public_key:
+            cipher = PKCS1_OAEP.new(self.public_key)
         else:
             return False
+
+        encrypted_data = cipher.encrypt(data)
+        return encrypted_data
 
     def sign(self, data):
         if self.private_key:
-            signature = rsa.sign(data, self.private_key, 'SHA-256')
-            return signature
+            try:
+                h = SHA256.new(data)
+                signature = pkcs1_15.new(self.private_key).sign(h)
+                return signature
+            except Exception:
+                return False
         else:
             return False
 
-
-# generate public and private keys with:
-# openssl genrsa -out private.pem 2048
-# openssl rsa -in private.pem -outform PEM -pubout -out public.pem
-
-
 # Example usage
-public_key = open('public.pem', 'r').read()
-private_key = open('private.pem', 'r').read()
+public_key = open('public.pem', 'rb').read()
+private_key = open('private.pem', 'rb').read()
 
-rsa_instance = Rsa(publicKey=public_key, privateKey=private_key)
+rsa_instance = Rsa(privateKey=private_key)
 
 # Encrypt data with the public key
 encrypted_data = rsa_instance.encrypt(b'This is a secret message')
