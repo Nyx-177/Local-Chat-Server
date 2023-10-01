e45721c1ee7aa8d42a1f0b678a9a4e990f9503ea
Nyx <necrownyx@outlook.com>
2023-10-01 16:24:38 +1000

    updated main.py to remove unneccisary function

--- client/main.py
+++ client/main.py
@@ -1,35 +1,8 @@
 import socket
-import concurrent.futures
 import threading
 
-server_ip = '192.168.0.13'
+server_ip = 'localhost'
 server_port = 14201
-
-def scan_server(ip):
-    try:
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect((ip, server_port))
-        print(f'Found server at {ip}.')
-    except (ConnectionRefusedError, TimeoutError):
-        print(f'Connection to {ip} failed.')
-        return None
-
-if server_ip == None:
-    print('Scanning for server...')
-    
-    # Generate a list of IP addresses to scan
-    ip_addresses = [f'192.168.{i}.{j}' for i in range(256) for j in range(256)]
-    
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        results = list(executor.map(scan_server, ip_addresses))
-    
-    # Filter out None values (failed connections) and get the first result
-    server_ip = next((ip for ip in results if ip is not None), None)
-    
-    if server_ip:
-        print(f'Found server at {server_ip}.')
-    else:
-        print('Server not found on the local network.')
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((server_ip, server_port))
